127829903d60a21c30fee6bbc6cf98c27e481d3a
TobiasMatthies <tbsmatthies@gmail.com>
2025-08-19 13:04:17 -0300

    executing video conversion in rq queue

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.api.urls')),
+    path('django-rq/', include('django_rq.urls'))
 ]
 
 if settings.DEBUG:
--- videos/signals.py
+++ videos/signals.py
@@ -2,12 +2,13 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from . utils import convert_video
 from . models import Video
+import django_rq
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print("video saved")
     if created:
        print("converting...")
-       convert_video(instance.video_file.path, "hd1080")
-       convert_video(instance.video_file.path, "hd720")
-       convert_video(instance.video_file.path, "hd480")
+       django_rq.enqueue(convert_video, instance.video_file.path, "hd1080")
+       django_rq.enqueue(convert_video, instance.video_file.path, "hd720")
+       django_rq.enqueue(convert_video, instance.video_file.path, "hd480")
