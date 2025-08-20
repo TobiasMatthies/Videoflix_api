1f09d4f6b006d869b5b848538b8a377dadf2cd4f
TobiasMatthies <tbsmatthies@gmail.com>
2025-08-20 11:07:14 -0300

    changed thumbnail url to file field

--- videos/api/serializers.py
+++ videos/api/serializers.py
@@ -3,6 +3,14 @@ from videos.models import Video
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    thumbnail_url = serializers.SerializerMethodField()
     class Meta:
         model = Video
         fields = ["id", "created_at", "title", "description", "thumbnail_url", "category"]
+
+
+    def get_thumbnail_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return request.build_absolute_uri(obj.thumbnail.url)
--- videos/models.py
+++ videos/models.py
@@ -4,6 +4,6 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    video_file = models.FileField(upload_to='videos/')
-    thumbnail_url = models.URLField(max_length=200)
+    video_file = models.FileField(upload_to="videos/")
+    thumbnail = models.FileField(upload_to="thumbnails/", default="")
     category = models.CharField(max_length=100)
