2fcbab3e307760aa276b58b2464515042a8ba76c
TobiasMatthies <tbsmatthies@gmail.com>
2025-08-25 10:54:52 -0300

    completed video playback

--- videos/api/urls.py
+++ videos/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VideoListAPIView, VideoPlaybackAPIView
+from .views import VideoListAPIView, VideoPlaybackAPIView, VideoSegmentView
 urlpatterns = [
     path('video/', VideoListAPIView.as_view(), name='video-list'),
     path('video/<int:movie_id>/<str:resolution>/index.m3u8', VideoPlaybackAPIView.as_view(), name='video-playback'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>', VideoSegmentView.as_view(), name='video-segment'),
 ]
--- videos/api/views.py
+++ videos/api/views.py
@@ -18,21 +18,44 @@ class VideoPlaybackAPIView(APIView):
     permission_classes = [IsAuthenticatedFromCookie]
 
     def get(self, request, *args, **kwargs):
-        print("Received request for video playback")
-        movie_id = kwargs.get('movie_id')
-        resolution = kwargs.get('resolution')
-        file_name = kwargs.get('file_name')
+        movie_id = kwargs.get("movie_id")
+        resolution = kwargs.get("resolution")
 
         try:
             video = Video.objects.get(id=movie_id)
             video_path = video.video_file.path
             video_dir = os.path.dirname(video_path)
 
-            hls_file_path = os.path.join(video_dir, f"hls_{os.path.splitext(os.path.basename(video_path))[0]}_{resolution}p", file_name)
+            hls_file_path = os.path.join(video_dir, f"hls_{os.path.splitext(os.path.basename(video_path))[0]}_hd{resolution}.m3u8")
 
             if os.path.exists(hls_file_path):
-                content_type = 'application/vnd.apple.mpegurl' if file_name.endswith('.m3u8') else 'video/MP2T'
-                return FileResponse(open(hls_file_path, 'rb'), content_type=content_type)
+                content_type = "application/vnd.apple.mpegurl"
+                return FileResponse(open(hls_file_path, "rb"), content_type=content_type)
+            else:
+                return Response({"error": "File not found"}, status=404)
+
+        except Video.DoesNotExist:
+            return Response({"error": "Video not found"}, status=404)
+
+class VideoSegmentView(APIView):
+    permission_classes = [IsAuthenticatedFromCookie]
+
+    def get(self, request, *args, **kwargs):
+        movie_id = kwargs.get("movie_id")
+        segment = kwargs.get("segment")
+
+        try:
+            video = Video.objects.get(id=movie_id)
+            video_path = video.video_file.path
+            video_dir = os.path.dirname(video_path)
+
+            print(segment)
+            hls_file_path = os.path.join(video_dir, segment)
+            print (hls_file_path)
+
+            if os.path.exists(hls_file_path):
+                content_type = "video/MP2T"
+                return FileResponse(open(hls_file_path, "rb"), content_type=content_type)
             else:
                 return Response({"error": "File not found"}, status=404)
 
